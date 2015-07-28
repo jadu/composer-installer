b5fc75039ffb68cf7b9d601e57f0df8f0f835600
Matt Dolan <matt.dolan@jadu.co.uk>
2015-07-28 11:48:23 +0100

    Simplify ob callback further

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -44,23 +44,23 @@ class EventDispatcher extends ComposerEventDispatcher {
         $output = array();
         $lines = explode("\n", $input);
         foreach ($lines as $line) {
-            if (mb_strlen($line) > Installer::CONSOLE_LINE_LENGTH) {
-                $words = mb_split("\s", $line);
-                $line = '';
-                while ($words) {
-                    if (mb_strlen($line) + mb_strlen(reset($words)) <= Installer::CONSOLE_LINE_LENGTH) {
-                        $word = array_shift($words);
-                        if ($line != '') {
-                            $line .= ' ';
-                        }
-                        $line .= $word;
-                    }
-                    else {
-                        $output[] = '    ' . $line;
-                        $line = '';
-                    }
-                }
-            }
+            // if (mb_strlen($line) > Installer::CONSOLE_LINE_LENGTH) {
+            //     $words = mb_split("\s", $line);
+            //     $line = '';
+            //     while ($words) {
+            //         if (mb_strlen($line) + mb_strlen(reset($words)) <= Installer::CONSOLE_LINE_LENGTH) {
+            //             $word = array_shift($words);
+            //             if ($line != '') {
+            //                 $line .= ' ';
+            //             }
+            //             $line .= $word;
+            //         }
+            //         else {
+            //             $output[] = '    ' . $line;
+            //             $line = '';
+            //         }
+            //     }
+            // }
             $output[] = '    ' . $line;
         }
         return implode("\n", $output);
