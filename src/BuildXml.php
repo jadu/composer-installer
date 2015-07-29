2fcc651133e2b0bd45d11c458247b93c69512108
Matt Dolan <matt.dolan@jadu.co.uk>
2015-07-30 00:49:22 +0100

    Add methods to read, parse, add and write build.xml

--- src/BuildXml.php
+++ src/BuildXml.php
@@ -17,28 +17,78 @@ class BuildXml {
     }
 
     /**
-     * Adds each of the file paths in the given array to the .gitignore,
+     * Adds each of the file paths in the given array to the files included in the package
      * if they are not already present there
      *
-     * File paths should be relative to the .gitignore file
-     * @param array[]string $files
+     * @param string $file
      */
-    public function addFiles()
+    public function addInclude($file)
     {
-        $addedCount = 0;
-        $lines = $this->read();
+        if (!$this->xml) return;
+        $this->includes[] = $file;
+    }
 
-        return $addedCount;
+    /**
+     * Adds each of the file paths in the given array to the files excluded from the package
+     * if they are not already present there
+     *
+     * @param string $file
+     */
+    public function addExclude($file)
+    {
+        if (!$this->xml) return;
+        $this->excludes[] = $file;
     }
 
     protected function read()
     {
+        if (!is_file($this->buildXmlPath)) {
+            $this->xml = null;
+            return;
+        }
+
         $this->xml = simplexml_load_file($this->buildXmlPath);
-        print_r($this->xml);
+        $this->includes = array();
+        $this->excludes = array();
+
+        if (isset($this->xml->fileset)) {
+            $this->parseFileset($this->xml->fileset);
+        }
+        else {
+            // load & parse default fileset from Meteor
+            // attach node to $this->xml
+        }
+    }
+
+    protected function parseFileset($filesetNode)
+    {
+        if (isset($filesetNode->include)) {
+            foreach ($filesetNode->include as $includeNode) {
+                $this->includes[] = (string)$includeNode['name'];
+            }
+        }
+        if (isset($filesetNode->exclude)) {
+            foreach ($filesetNode->exclude as $excludeNode) {
+                $this->excludes[] = (string)$excludeNode['name'];
+            }
+        }
     }
 
     protected function write()
     {
+        $filesetNode = new SimpleXmlElement('<fileset id="fileset.files" dir="${basedir}"></fileset>');
+        foreach ($this->includes as $includePath) {
+            $includeNode = new SimpleXmlElement('<include />');
+            $includeNode->addAttribute('name', $includePath);
+            $filesetNode->addChild($includeNode);
+        }
+        foreach ($this->excludes as $excludePath) {
+            $excludeNode = new SimpleXmlElement('<exclude />');
+            $excludeNode->addAttribute('name', $excludePath);
+            $filesetNode->addChild($excludeNode);
+        }
+        $this->xml['fileset'] = $filesetNode;
+
         return file_put_contents($this->buildXmlPath, $this->xml->asXML());
     }
 
