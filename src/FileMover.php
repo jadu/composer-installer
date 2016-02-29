6289ab15f102a6959c770e9051e39d71f6ce0c7b
Paul Bele <paul.bele@jadu.net>
2016-02-29 09:13:59 +0200

    Create the folder structure if a file is copied

--- src/FileMover.php
+++ src/FileMover.php
@@ -91,6 +91,15 @@ class FileMover {
             }
         }
         else {
+            //  Check if directory where the file will be copied exists first
+            if(!file_exists(dirname($dest))){
+                $success = $this->createFolder(dirname($dest));
+
+                if (!$success) {
+                    throw new RuntimeException("Failed to create folder for $dest");
+                }
+            }
+
             $success = copy($source, $dest);
             if (!$success) {
                 throw new RuntimeException("Failed to copy file to $dest");
@@ -105,6 +114,17 @@ class FileMover {
         }
 
         return true;
+    }
+
+    /**
+     * Creates a folder recursively for a given path
+     * @param $path
+     *
+     * @return bool
+     */
+    public function createFolder($path)
+    {
+        return mkdir($path, 0755, true);
     }
 
     /**
